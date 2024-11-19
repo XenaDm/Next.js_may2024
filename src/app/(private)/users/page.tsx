3eac0c3e6b4becb53c97e9252c73976df9002088
HTML Project <ksiushabliz@gmail.com>
2024-11-19 18:51:29 +0200

    lesson 12(next.js - 2) - клієнтські компоненти

--- src/app/(private)/users/page.tsx
+++ src/app/(private)/users/page.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import UsersComponent from "@/app/components/users/UsersComponent"
 
 const UsersPage = async () => {
-    const users = await  fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await  fetch('https://jsonplaceholder.typicode.com/users', {
+        // cache:'no-cache', //SSR
+        // cache:'force-cache', //SSR
+        next:{revalidate: 60}
+    })
         .then(value => value.json())
 
+//00:00:00
+//00:00:55 - cache
+//00:01:01 - revalidate
+//00:XX:XX - revalidate
 
     return (
         <div>
