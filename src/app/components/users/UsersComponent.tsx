214c0178fc464513075ea7ff3271a08566654d14
HTML Project <ksiushabliz@gmail.com>
2024-11-19 16:56:16 +0200

    lesson 12(next.js - 2) - підв'язка кожного окремого юзера до сторінки з усіма юзерами

---
+++ src/app/components/users/UsersComponent.tsx
@@ -0,0 +1,24 @@
+import React,{FC} from 'react';
+import Link from "next/link"
+
+type Props={
+    users: {id:number, name:string}[]
+}
+const UsersComponent:FC<Props> = ({users}) => {
+    return (
+        <div>
+            {
+                users.map(({id, name})=>(<div key={id}><Link href={
+                    {
+                        pathname: '/users/' + id,
+                    query:{xxx:JSON.stringify({id, name})}
+                    }
+                }
+                >{name}</Link>
+                </div>))
+            }
+        </div>
+    );
+};
+
+export default UsersComponent;
