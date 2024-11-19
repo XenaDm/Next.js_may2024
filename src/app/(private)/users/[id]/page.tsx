47e6691e019e5a68a6b7bf412055d444137bad42
HTML Project <ksiushabliz@gmail.com>
2024-11-19 18:32:26 +0200

    lesson 12(next.js - 2) - підв'язка кожного окремого юзера до сторінки з усіма юзерами, виведена уся інформація юзера з сервера +searchParams(only id, name of user)

--- src/app/(private)/users/[id]/page.tsx
+++ src/app/(private)/users/[id]/page.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React,{FC} from 'react';
 
-type Params = {
-    id:string
+
+type Params = Promise<{ id: string }>
+type SearchParams = Promise<{[key:string]: string | string[] | undefined}>
+type MyProps ={
+    params: Params;
+    searchParams:SearchParams
 }
-const UserPage = async ({params}:{params:Params}) => {
-    const user = await fetch('https://jsonplaceholder.typicode.com/users/'+params.id)
+const UserPage:FC<MyProps> = async ({params, searchParams}) => {
+    const {id} = await params;
+    const {xxx} = await searchParams;
+    const user = await fetch('https://jsonplaceholder.typicode.com/users/'+id)
         .then(res => res.json())
-
     return (
         <div>
             {JSON.stringify(user)}
+            <hr/>
+            {xxx}
         </div>
     );
 };
